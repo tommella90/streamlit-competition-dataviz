37aff0dd78be5a9cf42ae84e65e33650e0a768d9
tommaso.ramella <tommaso.ramella90@gmail.com>
2023-03-10 14:32:37 +0100

    something

--- eurobarometer.py
+++ eurobarometer.py
@@ -2,7 +2,7 @@ import pandas as pd
 import parquet
 
 #%%
-df = pd.read_stata('data/harmonised_EB_2004-2021.dta', convert_categoricals=False)
+#df = pd.read_stata('data/harmonised_EB_2004-2021.dta', convert_categoricals=False)
+#df.to_parquet("data_clean/eurobarometer.parquet.gzip", compression="gzip")
 
 #%%
-df.to_parquet("data_clean/eurobarometer.parquet.gzip", compression="gzip")
